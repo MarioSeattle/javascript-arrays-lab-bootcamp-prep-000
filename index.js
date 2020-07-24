42226cc426e6d619d9f4894e4a8930efddbba91b
Mario Gomez <mgomez15@seattlecentral.edu>
2020-07-24 17:21:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name){
+  
+ return kittens.push("Ralph");
+  
+}
+
+
+function destructivelyPrependKitten(name){
+  
+ return kittens.unshift("Bob");
+ 
+}
+
+function destructivelyRemoveLastKitten(name){
+  
+ return kittens.pop("Ralph");
+  
+}
+
+function destructivelyPrependKitten(name){
+  
+return kittens.unshift("Bob");
+ 
+}
+
+function destructivelyRemoveFirstKitten(name){
+  
+return kittens.shift("Bob");
+ 
+}
+
+function appendKitten(name){
+  
+return kittens.shift("coco");
+ 
+}
+
+
+
